8d3318b63b7173a78cbc2c7608adc7bf66cdbbe4
Trevsh <trevonshelby@outlook.com>
2024-10-18 19:33:15 -0500

    Add bcrypt and database

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
-import { express } from 'express'
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-const app = express()
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createPool({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+});
+
+app.post('/api/register', (req, res) => {
+	const { name, email, password } = req.body;
+
+	bcrypt.hash(password, 10, (err, hash) => {
+		if (err) return res.status(500).json({ message: 'Error hashing password!' });
+
+		const sql = 'INSERT INTO users (name, email, password) VALUES (?,?,?)';
+		db.query(sql, [name, email, hash], (err, result) => {
+			if (err) return res.status(500).json({ message: 'Error registering user' });
+			res.status(201).json({ message: 'User registered successfully' });
+		});
+	});
+});
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log(`Server is running on ${process.env.PORT | 5000}`);
+});
