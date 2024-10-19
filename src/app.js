e3cd272e9344c2a7cf8348859371163c6ef3b863
Trevsh <trevonshelby@outlook.com>
2024-10-19 10:12:53 -0500

    Add auth

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const authenticateToken = require('./middleware/auth');
+
 const express = require('express');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
@@ -26,14 +28,40 @@ app.post('/api/register', (req, res) => {
 		if (err) return res.status(500).json({ message: 'Error hashing password!' });
 
 		const sql = 'INSERT INTO users (name, email, password) VALUES (?,?,?)';
-		db.query(sql, [name, email, hash], (err, result) => {
+		db.query(sql, [name, email, hash], (err, res) => {
 			if (err) return res.status(500).json({ message: 'Error registering user' });
 			res.status(201).json({ message: 'User registered successfully' });
 		});
 	});
 });
 
-//app.post Login logic
+app.post('api/login', (req, res) => {
+	const { email, password } = req.body;
+
+	const sql = 'SELECT * FROM users WHERE email = ?'
+	db.query(sql, [email], (err, res) => {
+		if (err) return res.status(500).json({ message: 'Database Error. Email not found.' });
+		if (res.length === 0) res.status(400).json({ message: 'User not found' });
+
+		const user = res[0];
+
+		bcrypt.compare(password, user.password, (err, isMatch) => {
+			if (err) return res.status(500).json({ message: 'Error comparing passwords' });
+			if (!isMatch) return res.status(400).json({ message: 'Incorrect Password' });
+
+			//Generate JWT token
+			const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+				expiresIn: '1hr'
+			});
+
+			res.status(200).json({ message: 'Login Successful', token });
+		});
+	});
+});
+
+app.get('/api/protect-route', authenticateToken, (req, res) => {
+	res.json({ message: 'This route is protected', user: req.user });
+});
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on ${process.env.PORT | 5000}`);
