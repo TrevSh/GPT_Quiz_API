e059373e84aabeead972e5f168e214aebe27c3e0
Trevsh <trevonshelby@outlook.com>
2024-10-19 21:45:23 -0500

    Fix auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-	const token = req.headers['authorization'];
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+
 	if (!token) return res.status(401).json({ message: 'Access Denied' });
 
 	jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-		if (err) return res.status(403).json({ message: 'Invalid token' });
+		if (err) { return res.status(403).json({ message: 'Invalid token' }) };
+
 		req.user = user;	//Attach user data to req object
 		next();
 	});
